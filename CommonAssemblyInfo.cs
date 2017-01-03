a3db512b00675a9868dd260a201a8d3d1a8878e2
Matthew McAtamney-Greenwood <matthew.i.greenwood@gmail.com>
2017-01-03 17:39:39 +0000

    Separated common assembly info to simplify versioning when implementing CI

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("G & B Technologies")]
+[assembly: AssemblyProduct("PluginSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
